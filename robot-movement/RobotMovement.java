713ec0ef50a7ac232135c451081d94874aff6d1a
jaiswalakshay <jaiswal740@gmail.com>
2017-08-06 05:52:06 -0400

    robot-movement problem solution

---
+++ robot-movement/RobotMovement.java
@@ -0,0 +1,37 @@
+package com.whatfix.runner;
+
+public class RobotMovement {
+	private static int countNumberOfPath(int inputGrid[][], int row, int col) {
+		if (row < 0 || row > 3 || col < 0 || col > 3) {
+			return 0;
+		}
+
+		if (inputGrid[row][col]!=0) {
+			return 0;
+		}
+
+		if (row == 3 && col == 3) {
+			return 1;
+		}
+
+		inputGrid[row][col] = 1;
+
+		int numbeOfPaths = 0;
+	
+		numbeOfPaths += countNumberOfPath(inputGrid, row - 1, col);
+		numbeOfPaths += countNumberOfPath(inputGrid, row, col - 1);
+		numbeOfPaths += countNumberOfPath(inputGrid, row, col + 1);
+		numbeOfPaths += countNumberOfPath(inputGrid, row + 1, col);
+
+		inputGrid[row][col] = 0;
+
+		return numbeOfPaths;
+	}
+
+	public static void main(String[] args) {
+		int initialValue[][] = { { 0, 0, 0, 0 }, { 0, 0, 0, 0 },
+				{ 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+		System.out.println("Total number of paths :" + countNumberOfPath(initialValue, 0, 0));
+	}
+
+}
